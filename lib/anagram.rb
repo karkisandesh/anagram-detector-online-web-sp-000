2f471ab38c6bb4be4367385325bae5723831c52f
Sandesh Karki <karkisandesh@hotmail.com>
2019-02-07 00:53:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,11 +6,11 @@ class Anagram
   end
   
   def match(array_of_words)
-    result = [ ]
-    array_of_words.each_with_index do |word|
-      result  = word.split("").sort == words.split("").sort
+  
+    array_of_words.map do |word|
+      word.split("").sort == words.split("").sort
     end
-    result
+  
   end
   
 end
